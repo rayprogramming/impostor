bf2d25340f46ef70399f01c07f0baeca30826666
James Ray <rihoj1@gmail.com>
2022-12-31 21:47:14 -0500

    feat: fix variables

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
   default_tags {
     tags = {
       Project   = "Impostor"
@@ -36,8 +36,10 @@ module "impostor_server" {
   source             = "./modules/impostor-server"
   private_key        = "~/.ssh/amongus.pem"
   zone_id            = data.aws_route53_zone.selected.id
+  vpc_id             = module.vpc.vpc_id
+  instance_availability_zone = module.vpc.azs[0]
   instance_subnet    = module.vpc.private_subnets[0]
-  gameserver_subnets = [module.vpc.public_subnets]
-  matchmaker_subnets = [module.vpc.public_subnets]
+  gameserver_subnets = module.vpc.public_subnets
+  matchmaker_subnets = module.vpc.public_subnets
   instance_key_name  = "amongus"
 }
